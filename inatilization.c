6babc300f33a4c1c671f8984c844491491b1bae2
Nathan <55759030+NathanNT@users.noreply.github.com>
2020-12-12 18:16:55 +0100

    Delete inatilization.c

--- inatilization.c
+++
@@ -1,9 +0,0 @@
-#include "inatilization.h"
-
-Element* create_el(int data) {
-    Element* new_el = NULL;
-    new_el = (Element*)malloc(sizeof(Element));
-    new_el->data = data;
-    new_el->next = NULL;
-    return new_el;
-}
